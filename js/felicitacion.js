ccb5858ba4f1e3fa98361f5b28006c3565951faf
FridaEscamilla02 <fridaescamilla0207@gmail.com>
2024-03-09 23:41:17 -0600

    Correccione menores, fotos y botones

--- js/felicitacion.js
+++ js/felicitacion.js
@@ -12,7 +12,7 @@ window.onload = function() {
         if (nombre) {
             ctx.font = "bold 40px Times New Roman";
             ctx.fillStyle = "#042608";
-            ctx.fillText("¡Felicidades " + (nombre ? nombre : "jugador") + "!", 220, 90);
+            ctx.fillText("¡Felicidades " + (nombre ? nombre : "jugador") + "!", 230, 90);
             ctx.fillText("Has rescatado a todos los animales.", 120, 150);
             ctx.fillText("Gracias por tu ayuda y tu dedicación.", 100, 210);
             ctx.fillText("Todos estamos en deuda contigo.", 130, 290);
@@ -25,3 +25,9 @@ window.onload = function() {
     };
     fondo.src = '../img/background/marcoCarta.jpg'; 
 };
+
+var params = new URLSearchParams(window.location.search);
+var nombre = params.get('nombre');
+function reinicio(){
+    window.location.href = "game.html?nombre=" + encodeURIComponent(nombre);
+}
